23912950f2c5e756496ea85474bdac5ed26841d4
Mark Triggs <mark@dishevelled.net>
2015-09-15 13:31:52 +1000

    Add patch for ArchivesSpace PR 252

--- backend/lib/aspace_patches.rb
+++ backend/lib/aspace_patches.rb
@@ -17,6 +17,24 @@ class MARCModel < ASpaceExport::ExportModel
   def self.from_aspace_object(obj)
     self.new(obj)
    end
+
+
+  # https://github.com/archivesspace/archivesspace/pull/252
+  def self.assemble_controlfield_string(obj)
+    date = obj.dates[0] || {}
+    string = obj['system_mtime'].scan(/\d{2}/)[1..3].join('')
+    string += obj.level == 'item' && date['date_type'] == 'single' ? 's' : 'i'
+    string += date['begin'] ? date['begin'][0..3] : "    "
+    string += date['end'] ? date['end'][0..3] : "    "
+    string += "xx"
+    18.times { string += ' ' }
+    string += (obj.language || '|||')
+    string += ' d'
+
+    string
+  end
+
+
 end
 
 
